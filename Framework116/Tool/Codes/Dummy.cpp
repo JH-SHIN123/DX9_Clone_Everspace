70e85b0b95dbca1a3a7ba01713c3f85dd4270e23
DESKTOP-HL46KFK\willb <willbe012@naver.com>
2021-06-29 20:57:22 +0900

    ver 0.001 Object Tool Load 전까지 사용할 Dummy 추가

---
+++ Framework116/Tool/Codes/Dummy.cpp
@@ -0,0 +1,171 @@
+#include "pch.h"
+#include "..\Headers\Dummy.h"
+#include <MainFrm.h>
+#include "MainView.h"
+
+CDummy::CDummy(LPDIRECT3DDEVICE9 pDevice)
+	: CGameObject(pDevice)
+{
+}
+
+CDummy::CDummy(const CDummy & other)
+	: CGameObject(other)
+{
+}
+
+HRESULT CDummy::Ready_GameObject_Prototype()
+{
+	CGameObject::Ready_GameObject_Prototype();
+
+	return S_OK;
+}
+
+HRESULT CDummy::Ready_GameObject(void * pArg/* = nullptr*/)
+{
+	CGameObject::Ready_GameObject(pArg);
+
+	// For.Com_VIBuffer
+	if (FAILED(CGameObject::Add_Component(
+		EResourceType::Static,
+		L"Component_VIBuffer_CubeTexture",
+		L"Com_VIBuffer",
+		(CComponent**)&m_pVIBuffer)))
+	{
+		PRINT_LOG(L"Error", L"Failed To Add_Component Com_VIBuffer");
+		return E_FAIL;
+	}
+
+	// For.Com_Texture
+	if (FAILED(CGameObject::Add_Component(
+		EResourceType::NonStatic,
+		L"Component_Texture_Monster",
+		L"Com_Texture",
+		(CComponent**)&m_pTexture)))
+	{
+		PRINT_LOG(L"Error", L"Failed To Add_Component Com_Texture");
+		return E_FAIL;
+	}
+
+	// For.Com_Transform
+	TRANSFORM_DESC TransformDesc;
+	TransformDesc.vPosition = _float3(0.5f, 0.f, 0.5f);	
+
+	if (FAILED(CGameObject::Add_Component(
+		EResourceType::Static,
+		L"Component_Transform",
+		L"Com_Transform",
+		(CComponent**)&m_pTransform,
+		&TransformDesc)))
+	{
+		PRINT_LOG(L"Error", L"Failed To Add_Component Com_Transform");
+		return E_FAIL;
+	}
+
+	m_pTerrainBuffer = (CVIBuffer_TerrainTexture*)m_pManagement->Get_Component(L"Layer_Terrain", L"Com_VIBuffer");
+	Safe_AddRef(m_pTerrainBuffer);
+	if (nullptr == m_pTerrainBuffer)
+	{
+		PRINT_LOG(L"Error", L"m_pTerrainBuffer is nullptr");
+		return E_FAIL;
+	}
+
+	// For.Com_Collide
+	BOUNDING_SPHERE BoundingSphere;
+	BoundingSphere.fRadius = 1.f;
+
+	if (FAILED(CGameObject::Add_Component(
+		EResourceType::Static,
+		L"Component_CollideSphere",
+		L"Com_CollideSphere",
+		(CComponent**)&m_pCollide,
+		&BoundingSphere,
+		true)))
+	{
+		PRINT_LOG(L"Error", L"Failed To Add_Component Com_Transform");
+		return E_FAIL;
+	}
+
+	return S_OK;
+}
+
+_uint CDummy::Update_GameObject(_float fDeltaTime)
+{
+	CGameObject::Update_GameObject(fDeltaTime);	
+	Movement(fDeltaTime);
+
+	m_pTransform->Update_Transform();
+	m_pCollide->Update_Collide(m_pTransform->Get_TransformDesc().vPosition);
+	return NO_EVENT;
+}
+
+_uint CDummy::LateUpdate_GameObject(_float fDeltaTime)
+{
+	CGameObject::LateUpdate_GameObject(fDeltaTime);
+
+	if (FAILED(m_pManagement->Add_GameObject_InRenderer(ERenderType::NonAlpha, this)))
+		return UPDATE_ERROR;
+
+	return _uint();
+}
+
+_uint CDummy::Render_GameObject()
+{
+	CGameObject::Render_GameObject();
+
+	m_pDevice->SetTransform(D3DTS_WORLD, &m_pTransform->Get_TransformDesc().matWorld);
+	m_pTexture->Set_Texture(1);
+	m_pVIBuffer->Render_VIBuffer();
+
+#ifdef _DEBUG // Render Collide
+	m_pCollide->Render_Collide();
+#endif
+
+	return _uint();
+}
+
+_uint CDummy::Movement(_float fDeltaTime)
+{
+	_float3 vOutPos = m_pTransform->Get_State(EState::Position);
+	if (true == m_pTerrainBuffer->Is_OnTerrain(&vOutPos))
+	{
+		vOutPos.y += 0.5f;
+		m_pTransform->Set_Position(vOutPos);
+	}	
+
+	return _uint();
+}
+
+CDummy * CDummy::Create(LPDIRECT3DDEVICE9 pDevice)
+{
+	CDummy* pInstance = new CDummy(pDevice);
+	if (FAILED(pInstance->Ready_GameObject_Prototype()))
+	{
+		PRINT_LOG(L"Error", L"Failed To Create Player");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+CGameObject * CDummy::Clone(void * pArg/* = nullptr*/)
+{
+	CDummy* pClone = new CDummy(*this); /* 복사 생성자 호출 */
+	if (FAILED(pClone->Ready_GameObject(pArg)))
+	{
+		PRINT_LOG(L"Error", L"Failed To Clone Player");
+		Safe_Release(pClone);
+	}
+
+	return pClone;
+}
+
+void CDummy::Free()
+{
+	Safe_Release(m_pTerrainBuffer);
+	Safe_Release(m_pVIBuffer);
+	Safe_Release(m_pTransform);
+	Safe_Release(m_pTexture);
+	Safe_Release(m_pCollide);
+
+	CGameObject::Free();
+}
