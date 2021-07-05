d50002b2647ef35e998e00e7ee838292fae90afd
DESKTOP-LEIKETP\BLUE <aldald1318@gmail.com>
2021-07-05 22:02:16 +0900

    ver eff 0.002 디버깅 / 릴리즈모드 wstring& 오류 수정

--- Framework116/Client/Codes/EffectHandler.cpp
+++ Framework116/Client/Codes/EffectHandler.cpp
@@ -6,10 +6,10 @@ HRESULT CEffectHandler::Add_Layer_Effect_Explosion(const _float3& _vPos, const _
 	PARTICLESYSTEM_DESC pSystemDesc;
 	pSystemDesc.wstrTexturePrototypeTag = L"Component_Texture_Fire";
 	pSystemDesc.iNumParticles = 100;
-	pSystemDesc.tResetAttribute.fParticleSize = 10.f;
-	pSystemDesc.tResetAttribute.fParticleSpeed = 40.f;
-	pSystemDesc.tResetAttribute.fParticleAlphaFadeSpeed = 0.02f;
-	pSystemDesc.tResetAttribute.fLifeTime = 1.f;
+	pSystemDesc.tResetAttribute.fParticleSize = 10.f * _fSize;
+	pSystemDesc.tResetAttribute.fParticleSpeed = 30.f;
+	pSystemDesc.tResetAttribute.fParticleAlphaFadeSpeed = 0.04f;
+	pSystemDesc.tResetAttribute.fLifeTime = 1.5f;
 	pSystemDesc.tTransformDesc.vPosition = _vPos;
 	pSystemDesc.tResetAttribute.vColorRed_RandomRange = { 1.f,1.f };
 	pSystemDesc.tResetAttribute.vColorGreen_RandomRange = { 1.f,1.f };
@@ -25,11 +25,14 @@ HRESULT CEffectHandler::Add_Layer_Effect_Explosion(const _float3& _vPos, const _
 	}
 
 	pSystemDesc.wstrTexturePrototypeTag = L"Component_Texture_Smoke";
-	pSystemDesc.iNumParticles = 100;
-	pSystemDesc.tResetAttribute.fParticleSize = 15.f;
+	pSystemDesc.iNumParticles = 50;
+	pSystemDesc.tResetAttribute.fParticleSize = 15.f * _fSize;
 	pSystemDesc.tResetAttribute.fParticleSpeed = 20.f;
 	pSystemDesc.tResetAttribute.fParticleAlphaFadeSpeed = 0.01f;
-	pSystemDesc.tResetAttribute.fLifeTime = 2.5f;
+	pSystemDesc.tResetAttribute.fLifeTime = 2.f;
+	pSystemDesc.tResetAttribute.vColorRed_RandomRange = { 0.f,1.f };
+	pSystemDesc.tResetAttribute.vColorGreen_RandomRange = { 0.f,1.f };
+	pSystemDesc.tResetAttribute.vColorBlue_RandomRange = { 0.f,1.f };
 
 	if (FAILED(CManagement::Get_Instance()->Add_GameObject_InLayer(
 		EResourceType::NonStatic,
